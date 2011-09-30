53b70b4f73d1975e95763fcfa1ea1e9263794fd2
Fredi Machado <fredisoft@gmail.com>
2011-09-30 11:28:44 -0300

    Fix build warning

--- src/IRCClient.cpp
+++ src/IRCClient.cpp
@@ -12,7 +12,7 @@
 std::vector<std::string> split(std::string const& text, char sep)
 {
     std::vector<std::string> tokens;
-    int start = 0, end = 0;
+    size_t start = 0, end = 0;
     while ((end = text.find(sep, start)) != std::string::npos)
     {
         tokens.push_back(text.substr(start, end - start));
