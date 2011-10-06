9852f6106b1587c7bb6519bb61adc29e5393d9e1
Fredi Machado <fredisoft@gmail.com>
2011-10-06 15:47:59 -0300

    Now the client accepts some commands:
    /msg target message to send
    /join #somechannel [password]
    /leave #somechannel
    /ctcp target version

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,6 +18,8 @@
 #include <iostream>
 #include <signal.h>
 #include <cstdlib>
+#include <map>
+#include <algorithm>
 #include "Thread.h"
 #include "IRCClient.h"
 
@@ -28,14 +30,119 @@ void signalHandler(int signal)
     running = false;
 }
 
+class ConsoleCommandHandler
+{
+public:
+    bool AddCommand(std::string name, int argCount, void (*handler)(std::string /*params*/, IRCClient* /*client*/))
+    {
+        CommandEntry entry;
+        entry.argCount = argCount;
+        entry.handler = handler;
+        std::transform(name.begin(), name.end(), name.begin(), towlower);
+        _commands.insert(std::pair<std::string, CommandEntry>(name, entry));
+        return true;
+    }
+
+    void ParseCommand(std::string command, IRCClient* client)
+    {
+        if (_commands.empty())
+        {
+            std::cout << "No commands available." << std::endl;
+            return;
+        }
+
+        if (command[0] == '/')
+            command = command.substr(1); // Remove the slash
+
+        std::string name = command.substr(0, command.find(" "));
+        std::string args = command.substr(command.find(" ") + 1);
+        int argCount = std::count(args.begin(), args.end(), ' ');
+
+        std::transform(name.begin(), name.end(), name.begin(), towlower);
+
+        std::map<std::string, CommandEntry>::const_iterator itr = _commands.find(name);
+        if (itr == _commands.end())
+        {
+            std::cout << "Command not found." << std::endl;
+            return;
+        }
+
+        if (++argCount < itr->second.argCount)
+        {
+            std::cout << "Insuficient arguments." << std::endl;
+            return;
+        }
+
+        (*(itr->second.handler))(args, client);
+    }
+
+private:
+    struct CommandEntry
+    {
+        int argCount;
+        void (*handler)(std::string /*arguments*/, IRCClient* /*client*/);
+    };
+
+    std::map<std::string, CommandEntry> _commands;
+};
+
+ConsoleCommandHandler commandHandler;
+
+void msgCommand(std::string arguments, IRCClient* client)
+{
+    std::string to = arguments.substr(0, arguments.find(" "));
+    std::string text = arguments.substr(arguments.find(" ") + 1);
+
+    std::cout << "To " + to + ": " + text << std::endl;
+    client->SendIRC("PRIVMSG " + to + " :" + text);
+};
+
+void joinCommand(std::string channel, IRCClient* client)
+{
+    if (channel[0] != '#')
+        channel = "#" + channel;
+
+    client->SendIRC("JOIN " + channel);
+}
+
+void partCommand(std::string channel, IRCClient* client)
+{
+    if (channel[0] != '#')
+        channel = "#" + channel;
+
+    client->SendIRC("PART " + channel);
+}
+
+void ctcpCommand(std::string arguments, IRCClient* client)
+{
+    std::string to = arguments.substr(0, arguments.find(" "));
+    std::string text = arguments.substr(arguments.find(" ") + 1);
+
+    std::transform(text.begin(), text.end(), text.begin(), towupper);
+
+    client->SendIRC("PRIVMSG " + to + " :\001" + text + "\001");
+}
+
 ThreadReturn inputThread(void* client)
 {
     std::string command;
 
+    commandHandler.AddCommand("msg", 2, &msgCommand);
+    commandHandler.AddCommand("join", 1, &joinCommand);
+    commandHandler.AddCommand("part", 1, &partCommand);
+    commandHandler.AddCommand("ctcp", 2, &ctcpCommand);
+
     while(true)
     {
         getline(std::cin, command);
-        ((IRCClient*)client)->SendIRC(command);
+        if (command == "")
+            continue;
+
+        if (command[0] == '/')
+            commandHandler.ParseCommand(command, (IRCClient*)client);
+        else
+            ((IRCClient*)client)->SendIRC(command);
+
         if (command == "quit")
             break;
     }
